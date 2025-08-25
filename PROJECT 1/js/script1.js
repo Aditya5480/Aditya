4bbb449bb46fc21e4af1ef647151eca1b478b305
Aditya Kumar <adityakr90990@gmail.com>
2025-08-25 05:36:22 -0700

    Delete PROJECT 1 directory

--- PROJECT 1/js/script1.js
+++
@@ -1,8 +0,0 @@
-// JavaScript to handle button click
-document.addEventListener("DOMContentLoaded", function () {
-  const registerBtn = document.querySelector(".hero button");
-
-  registerBtn.addEventListener("click", function () {
-    alert("Thank you for your interest! Our team will contact you soon.");
-  });
-});
